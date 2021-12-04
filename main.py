03c7a410a5566a44c4478c965d4333bab35e95e4
Vera <catharsis1607@gmail.com>
2021-12-04 03:41:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Путь к файлу')
+
+# Пример результата:
+
+# Введите ваше имя: Sasha
+# Введите имя файла: text
+# C:/Sasha/docs/folder/text.txt
+
+user = input('Введите ваше имя: ')
+file_name = input('Введите имя файла: ')
+
+print('C:/' + user + '/docs/folder/' + file_name + '.txt')
